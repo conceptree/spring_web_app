efeef2328cd43f7c9b7436c5a83cfeea55b074c9
Nuno Rodrigues <nunorodrigues@conceptree.com>
2021-01-17 19:50:07 +0000

    first commit

---
+++ src/test/java/com/sdp/webapp/WebappApplicationTests.java
@@ -0,0 +1,20 @@
+package com.sdp.webapp;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.stereotype.Controller;
+
+@SpringBootTest
+class WebappApplicationTests {
+
+    @Autowired
+    private WebController controller;
+
+    @Test
+    void contextLoads() {
+        Assertions.assertThat(controller).isNotNull();
+    }
+
+}
